30365d8c78387c930c4264fad8c1bc8129b19b08
K. Semenza <ksemenza13@gmail.com>
2020-01-28 11:35:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom'
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="./components/About.js" component={About} >
+      <About/>      </Route>
+    <Route path={"./components/Contact.js"} component={Contact} />
   </div>
 );
 
